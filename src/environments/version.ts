254780f24094442d7ab38b66f01c6bb53f5366bd
Jan R. Biasi <janbiasi@users.noreply.github.com>
2020-01-20 23:10:53 +0100

    chore: delete example version

--- src/environments/version.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file will be generated automatically on prestart/prebuild
- */
-export const VERSION = 'not-generated';
-export const HASH = 'not-generated';
-export const TAG = 'not-generated';
-export const SUFFIX = 'not-generated';
-
-export default {};
