8596844bc332bddcc20112dc37994c2b7701cb6b
Jan Biasi <biasijan@gmail.com>
2020-03-22 19:59:54 +0100

    feat: 🎸 finalize cache implementation

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { onError } from 'apollo-link-error';
@@ -7,6 +7,7 @@ import { persistCache } from 'apollo-cache-persist';
 import { Storage } from '@ionic/storage';
 import { environment } from '../environments/environment';
 import { LogService } from './services/log.service';
+import { ApolloClientOptions } from 'apollo-client';
 
 const uri = environment.graphQLHostURL;
 
@@ -30,17 +31,6 @@ export function createApolloInitializer(httpLink: HttpLink, log: LogService, sto
 
 	const cache = new InMemoryCache();
 
-	persistCache({
-		cache,
-		key: 'rk-gql-cache',
-		storage: {
-			setItem: (key, data) => storage.set(key, data),
-			getItem: (key) => storage.get(key),
-			removeItem: (key) => storage.remove(key),
-		},
-		trigger: 'background',
-	});
-
 	return {
 		link,
 		cache,
@@ -52,6 +42,24 @@ export function createApolloInitializer(httpLink: HttpLink, log: LogService, sto
 @NgModule({
 	exports: [ApolloModule, HttpLinkModule],
 	providers: [
+		{
+			provide: APP_INITIALIZER,
+			useFactory: (opts: ApolloClientOptions<any>, storage: Storage) => {
+				return async () =>
+					persistCache({
+						cache: opts.cache,
+						key: 'rk-gql-cache',
+						storage: {
+							setItem: (key, data) => storage.set(key, data),
+							getItem: (key) => storage.get(key),
+							removeItem: (key) => storage.remove(key),
+						},
+						trigger: 'background',
+					});
+			},
+			multi: true,
+			deps: [APOLLO_OPTIONS, Storage],
+		},
 		{
 			provide: APOLLO_OPTIONS,
 			useFactory: createApolloInitializer,
