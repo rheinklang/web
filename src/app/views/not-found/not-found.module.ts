2d55ac911199dbe146e48b7ec2f42b05bf48ce27
Jan Biasi <jan.biasi@namics.com>
2020-04-30 15:10:21 +0200

    fix: 🐛 cleanup no content module

--- src/app/views/not-found/not-found.module.ts
+++ src/app/views/not-found/not-found.module.ts
@@ -2,15 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../../shared.module';
-// tslint:disable-next-line:max-line-length
-import { ContentLoaderTeaserSponsorComponent } from '../../components/atoms/content-loader-teaser-sponsor/content-loader-teaser-sponsor.component';
-import { TeaserSponsorComponent } from '../../components/molecules/teaser-sponsor/teaser-sponsor.component';
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
 
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	declarations: [NotFoundComponent, ContentLoaderTeaserSponsorComponent, TeaserSponsorComponent],
+	declarations: [NotFoundComponent],
 	imports: [CommonModule, SharedModule, NotFoundRoutingModule],
 })
 export class NotFoundModule {}
