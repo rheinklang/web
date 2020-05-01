f4643fc10b8152579b3440102ab1b3bebd91d146
Jan Biasi <jan.biasi@namics.com>
2020-05-01 15:47:56 +0200

    fix: 🐛 duplicate module refs

--- src/app/views/event-overview/event-overview.module.ts
+++ src/app/views/event-overview/event-overview.module.ts
@@ -2,15 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../../shared.module';
-// tslint:disable-next-line:max-line-length
-import { ContentLoaderTeaserEventComponent } from '../../components/atoms/content-loader-teaser-event/content-loader-teaser-event.component';
-import { TeaserEventComponent } from '../../components/molecules/teaser-event/teaser-event.component';
 import { EventOverviewRoutingModule } from './event-overview-routing.module';
 import { EventOverviewComponent } from './event-overview.component';
 
 @NgModule({
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	declarations: [EventOverviewComponent, TeaserEventComponent, ContentLoaderTeaserEventComponent],
+	declarations: [EventOverviewComponent],
 	imports: [CommonModule, SharedModule, EventOverviewRoutingModule],
 })
 export class EventOverviewModule {}
