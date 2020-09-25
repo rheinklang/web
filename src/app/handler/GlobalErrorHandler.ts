dc528c73d343d670fa85e96e513ef5bc44d87e41
Jan R. Biasi <janbiasi@users.noreply.github.com>
2020-09-26 01:05:10 +0200

    fix: various issues (#53)

--- src/app/handler/GlobalErrorHandler.ts
+++ src/app/handler/GlobalErrorHandler.ts
@@ -2,6 +2,7 @@ import { ErrorHandler, Injectable } from '@angular/core';
 import { LogService } from '../services/log.service';
 import { MatSnackBar } from '@angular/material';
 import { ERROR_NOTIFICATION_CONTENT, ERROR_NOTIFICATION_ACTION } from '../config/notifications';
+import { environment } from '../../environments/environment';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,12 +12,16 @@ export class GlobalErrorHandler implements ErrorHandler {
 	constructor(private log: LogService, private snackbar: MatSnackBar) {}
 
 	public handleError(error: any) {
-		(window as any).e = error;
+		if (!environment.production) {
+			(window as any).e = error;
+			console.log(error);
+		}
+
 		this.log.traceError(
 			'GlobalErrorHandler',
 			error || {
 				name: 'GlobalErrorHandler',
-				message: `Unknown application error`,
+				message: environment.production ? `Unknown application error` : `${error}`,
 				code: 500,
 			}
 		);
